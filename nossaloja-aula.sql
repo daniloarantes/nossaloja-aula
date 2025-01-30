6a705994b5b1608f95137615390a5e58c5a708fc
Danilo Arantes <daniloarantes@gmail.com>
2025-01-30 13:59:06 -0300

    Script para criação do banco

---
+++ nossaloja-aula.sql
@@ -0,0 +1,408 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: database:3306
+-- Tempo de geração: 30/01/2025 às 16:56
+-- Versão do servidor: 8.4.3
+-- Versão do PHP: 8.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `nossaloja-aula`
+--
+CREATE DATABASE IF NOT EXISTS `nossaloja-aula` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `nossaloja-aula`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `auth_groups_users`
+--
+
+CREATE TABLE `auth_groups_users` (
+  `id` int UNSIGNED NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
+  `group` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `auth_groups_users`
+--
+
+INSERT INTO `auth_groups_users` (`id`, `user_id`, `group`, `created_at`) VALUES
+(1, 1, 'user', '2025-01-30 00:26:19');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `auth_identities`
+--
+
+CREATE TABLE `auth_identities` (
+  `id` int UNSIGNED NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `secret` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `secret2` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `expires` datetime DEFAULT NULL,
+  `extra` text COLLATE utf8mb4_general_ci,
+  `force_reset` tinyint(1) NOT NULL DEFAULT '0',
+  `last_used_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `auth_identities`
+--
+
+INSERT INTO `auth_identities` (`id`, `user_id`, `type`, `name`, `secret`, `secret2`, `expires`, `extra`, `force_reset`, `last_used_at`, `created_at`, `updated_at`) VALUES
+(1, 1, 'email_password', NULL, 'eliaslavolpe@gmail.com', '$2y$12$0XQdcx.O6csquPtPJtE6uuSipHPHAE6ZYEEO2wMobmOMTwn40okB2', NULL, NULL, 0, '2025-01-30 16:54:23', '2025-01-30 00:26:19', '2025-01-30 16:54:23');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `auth_logins`
+--
+
+CREATE TABLE `auth_logins` (
+  `id` int UNSIGNED NOT NULL,
+  `ip_address` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_agent` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `id_type` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `identifier` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_id` int UNSIGNED DEFAULT NULL,
+  `date` datetime NOT NULL,
+  `success` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `auth_logins`
+--
+
+INSERT INTO `auth_logins` (`id`, `ip_address`, `user_agent`, `id_type`, `identifier`, `user_id`, `date`, `success`) VALUES
+(1, '10.89.0.4', 'Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0', 'email_password', 'eliaslavolpe@gmail.com', 1, '2025-01-30 00:46:13', 1),
+(2, '10.89.0.4', 'Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0', 'email_password', 'eliaslavolpe@gmail.com', 1, '2025-01-30 16:54:23', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `auth_permissions_users`
+--
+
+CREATE TABLE `auth_permissions_users` (
+  `id` int UNSIGNED NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
+  `permission` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `auth_remember_tokens`
+--
+
+CREATE TABLE `auth_remember_tokens` (
+  `id` int UNSIGNED NOT NULL,
+  `selector` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `hashedValidator` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
+  `expires` datetime NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `auth_token_logins`
+--
+
+CREATE TABLE `auth_token_logins` (
+  `id` int UNSIGNED NOT NULL,
+  `ip_address` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_agent` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `id_type` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `identifier` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_id` int UNSIGNED DEFAULT NULL,
+  `date` datetime NOT NULL,
+  `success` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` bigint UNSIGNED NOT NULL,
+  `version` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `class` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `group` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `namespace` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `time` int NOT NULL,
+  `batch` int UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
+(1, '2020-12-28-223112', 'CodeIgniter\\Shield\\Database\\Migrations\\CreateAuthTables', 'default', 'CodeIgniter\\Shield', 1738196025, 1),
+(2, '2021-07-04-041948', 'CodeIgniter\\Settings\\Database\\Migrations\\CreateSettingsTable', 'default', 'CodeIgniter\\Settings', 1738196025, 1),
+(3, '2021-11-14-143905', 'CodeIgniter\\Settings\\Database\\Migrations\\AddContextColumn', 'default', 'CodeIgniter\\Settings', 1738196025, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id` int NOT NULL,
+  `produto` varchar(50) NOT NULL,
+  `descricao` varchar(100) NOT NULL,
+  `preco` decimal(10,0) NOT NULL,
+  `imagem` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Despejando dados para a tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `produto`, `descricao`, `preco`, `imagem`) VALUES
+(1, 'Elias', 'Mito', 111, '1738193741_63a93eaba198e8499a84.jpg'),
+(2, 'Ventilador ARno', 'Ventilador de teto Arno', 550, '1738194798_5732b764aba49be4fa74.jpg'),
+(3, 'teste', 'teste', 113, '1738195314_9598ac51e8e28bfdf6be.jpg'),
+(4, 'Danilo', 'danilo', 11, '1738195509_36c0dc339c7dda47ce03.jpg'),
+(5, 'aaaaaaaaaa', 'aaaaa', 111, '1738196051_b09bfb1ff7c17567ad3a.jpg'),
+(6, 'Novo Teste', 'aa', 111, '1738199068_432528dae69c6bc8ac98.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `settings`
+--
+
+CREATE TABLE `settings` (
+  `id` int NOT NULL,
+  `class` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `key` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `value` text COLLATE utf8mb4_general_ci,
+  `type` varchar(31) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'string',
+  `context` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int UNSIGNED NOT NULL,
+  `username` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `status` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `status_message` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0',
+  `last_active` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `status`, `status_message`, `active`, `last_active`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'elias', NULL, NULL, 1, '2025-01-30 16:54:23', '2025-01-30 00:26:18', '2025-01-30 00:26:19', NULL);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `auth_groups_users`
+--
+ALTER TABLE `auth_groups_users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `auth_groups_users_user_id_foreign` (`user_id`);
+
+--
+-- Índices de tabela `auth_identities`
+--
+ALTER TABLE `auth_identities`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `type_secret` (`type`,`secret`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Índices de tabela `auth_logins`
+--
+ALTER TABLE `auth_logins`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_type_identifier` (`id_type`,`identifier`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Índices de tabela `auth_permissions_users`
+--
+ALTER TABLE `auth_permissions_users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `auth_permissions_users_user_id_foreign` (`user_id`);
+
+--
+-- Índices de tabela `auth_remember_tokens`
+--
+ALTER TABLE `auth_remember_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `selector` (`selector`),
+  ADD KEY `auth_remember_tokens_user_id_foreign` (`user_id`);
+
+--
+-- Índices de tabela `auth_token_logins`
+--
+ALTER TABLE `auth_token_logins`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_type_identifier` (`id_type`,`identifier`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Índices de tabela `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `auth_groups_users`
+--
+ALTER TABLE `auth_groups_users`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `auth_identities`
+--
+ALTER TABLE `auth_identities`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `auth_logins`
+--
+ALTER TABLE `auth_logins`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `auth_permissions_users`
+--
+ALTER TABLE `auth_permissions_users`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `auth_remember_tokens`
+--
+ALTER TABLE `auth_remember_tokens`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `auth_token_logins`
+--
+ALTER TABLE `auth_token_logins`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `auth_groups_users`
+--
+ALTER TABLE `auth_groups_users`
+  ADD CONSTRAINT `auth_groups_users_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `auth_identities`
+--
+ALTER TABLE `auth_identities`
+  ADD CONSTRAINT `auth_identities_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `auth_permissions_users`
+--
+ALTER TABLE `auth_permissions_users`
+  ADD CONSTRAINT `auth_permissions_users_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `auth_remember_tokens`
+--
+ALTER TABLE `auth_remember_tokens`
+  ADD CONSTRAINT `auth_remember_tokens_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
