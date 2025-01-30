53957d855a6ee883abfc6d76e9e9d4dd3e0b208f
Danilo Arantes <daniloarantes@gmail.com>
2025-01-29 21:14:39 -0300

    Configurações shield

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'daniloarantes@gmail.com';
+    public string $fromName   = 'Danilo Arantes';
     public string $recipients = '';
 
     /**
